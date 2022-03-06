a5dfdde7d32650c50ed21a462618c3a46c62b12e
Mike <mike@Zipcoders-MacBook-Pro.local>
2022-03-05 19:03:13 -0500

    all tests pass

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,68 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private final Character[][] game;
+
+
     public Board(Character[][] matrix) {
+        game = matrix;
     }
+
 
     public Boolean isInFavorOfX() {
-        return null;
+        if (game[0][0] == 'X' && game[0][1] == 'X' && game[0][2] == 'X') {
+
+        } else if (game[1][0] == 'X' && game[1][1] == 'X' && game[1][2] == 'X') {
+            return true;
+        } else if (game[2][0] == 'X' && game[2][1] == 'X' && game[2][2] == 'X') {
+            return true;
+        } if (game[0][0] == 'X' && game[1][1] == 'X' && game[2][2] == 'X') {
+            return true;
+        } else if (game[2][0] == 'X' && game[1][1] == 'X' && game[0][2] == 'X') {
+            return true;
+        } else if (game[0][0] == 'X' && game[1][0] == 'X' && game[2][0] == 'X') {
+            return true;
+        } else if (game[0][1] == 'X' && game[1][1] == 'X' && game[2][1] == 'X') {
+            return true;
+        } else return game[0][2] == 'X' && game[1][2] == 'X' && game[2][2] == 'X';
     }
+
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        if (game[0][0] == 'O' && game[0][1] == 'O' && game[0][2] == 'O') {
+            return true;
+        } else if (game[1][0] == 'O' && game[1][1] == 'O' && game[1][2] == 'O') {
+            return true;
+        } else if (game[2][0] == 'O' && game[2][1] == 'O' && game[2][2] == 'O') {
+            return true;
+        } else if (game[0][0] == 'O' && game[1][1] == 'O' && game[2][2] == 'O') {
+            return true;
+        } else if (game[2][0] == 'O' && game[1][1] == 'O' && game[0][2] == 'O') {
+            return true;
+        } else if (game[0][0] == 'O' && game[1][0] == 'O' && game[2][0] == 'O') {
+            return true;
+        } else if (game[0][1] == 'O' && game[1][1] == 'O' && game[2][1] == 'O') {
+            return true;
+        } else return game[0][2] == 'O' && game[1][2] == 'O' && game[2][2] == 'O';
     }
 
+
     public Boolean isTie() {
-        return null;
+
+        return !isInFavorOfX() && !isInFavorOfO();
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfX()) {
+            return "X";
+        } else if (isInFavorOfO()) {
+            return "O";
+        } else {
+            return "";
+        }
     }
-
 }
+
+
+//init game state......no winner, plyer x winner, player o winner. change game state based on that. boolean?
